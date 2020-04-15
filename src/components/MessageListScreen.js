03f0171f53cd4da383a33d495429fa9fce23cb3a
楊育瑄 <yuhsuanya@gmail.com>
2020-04-16 00:52:38 +0800

    posts.json

---
+++ src/components/MessageListScreen.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Dimensions, FlatList , Image ,StyleSheet,Text,View} from 'react-native';
+import Feather from '@expo/vector-icons/Feather';
+import MessageListData from '../json/messageList';
+
+const screenWidth = Math.round(Dimensions.get('window').width);
